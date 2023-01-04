11d19381d0fe1c7ebb3a4dc58843062d6818fe84
shezhangzhang <ischenkan@outlook.com>
2023-01-04 15:48:49 +0800

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-J0YBZWY3N7`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J0YBZWY3N7', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
