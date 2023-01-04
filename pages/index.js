25ef7c18f55a265b068fa6bee55879ff40c8b50c
shezhangzhang <ischenkan@outlook.com>
2023-01-04 15:13:42 +0800

    post pinyin blog

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
@@ -15,12 +14,8 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-  console.log("allPostsData", allPostsData);
   return (
     <Layout home>
-      {/* Keep the existing code here */}
-
-      {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
